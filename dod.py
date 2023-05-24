367301c0ad27fd15504c9749010591ee18e741a1
Muhammad Hammad <133936014+HammadKhan245@users.noreply.github.com>
2023-05-24 10:45:27 +0500

    Add files via upload

---
+++ dod.py
@@ -0,0 +1,19 @@
+import os, platform, time, sys
+ 
+try:
+ 
+    import requests
+ 
+except:
+ 
+    os.system('pip install requests')
+ 
+os.system('git pull --quiet 2>/dev/null')
+ 
+bit = platform.architecture()[0]
+ 
+if bit == '64bit':
+ 
+    from dod64 import menu
+ 
+    menu()
